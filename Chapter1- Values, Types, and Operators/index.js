59fe7319fd54ff312f0caa31ce63126ef96c1222
Cherieliuc Carolina <carolinageorgiana30@outlook.com>
2020-08-27 13:03:33 +0300

    first commit

---
+++ Chapter1- Values, Types, and Operators/index.js
@@ -0,0 +1,17 @@
+//Unary operators
+
+console.log(typeof 4.5);
+
+console.log(typeof "x")
+
+console.log(-(10-3))
+
+// boolean values 
+
+console.log(3>2)
+
+console.log("Itchy"!= "Scratchy")
+
+console.log("True and false is:", true && false)
+
+console.log("False or true is:", false || true)
